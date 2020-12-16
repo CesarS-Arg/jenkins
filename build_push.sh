c65459800ef7318b186c6e69c3702b8c16f1300e
Cesar Sosa <cesar.sosa.1984@gmail.com>
2020-12-16 20:51:25 -0300

    Delete build_push.sh

--- build_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 394363795880.dkr.ecr.us-west-2.amazonaws.com
-docker build -t jenkins/jenkins-ci .
-docker tag jenkins/jenkins-ci:latest 394363795880.dkr.ecr.us-west-2.amazonaws.com/jenkins/jenkins-ci:latest
-docker push 394363795880.dkr.ecr.us-west-2.amazonaws.com/jenkins/jenkins-ci:latest
